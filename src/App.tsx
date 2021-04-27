fcc234a2199a7e4314f9a745049b0b4b1a409635
makhatadze <vitomaxatadze@gmail.com>
2021-04-27 09:54:47 +0400

    Show countries list.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {CountryType} from "./types";
+import axios from "axios";
+import Loading from "./components/Loading";
+import Country from "./components/Country";
 
 function App() {
-  return (
-    <div>
-      Countries app with typescript
-    </div>
-  );
+    const [countries, setCountries] = useState<CountryType[]>([])
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const getCountries = async () => {
+        setLoading(true);
+
+        try {
+            const {data} = await axios.get<CountryType[]>(
+                'https://restcountries.eu/rest/v2/all'
+            );
+            setCountries(data);
+        } catch (err) {
+            console.log('Error: ', err)
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getCountries().then();
+    }, [])
+
+    return (
+        <div>
+            <Loading loading={loading}>
+                {countries.map((country) => {
+                    return <Country key={country.name} country={country}/>
+                })}
+            </Loading>
+        </div>
+    );
 }
 
 export default App;
