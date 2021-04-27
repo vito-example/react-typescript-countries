218e3721e221e47cd1c63f99a7b91684211c1ac0
makhatadze <vitomaxatadze@gmail.com>
2021-04-27 09:40:25 +0400

    Declare interface of CountryType and export it.

--- src/types.ts
+++ src/types.ts
@@ -27,3 +27,30 @@ export interface RegionalBloc {
     otherAcronyms: string[];
     otherNames: string[];
 }
+
+export interface CountryType {
+    name: string;
+    topLevelDomain: string[];
+    alpha2Code: string;
+    alpha3Code: string;
+    callingCodes: string[];
+    capital: string;
+    altSpellings: string[];
+    region: string;
+    subregion: string;
+    population: number;
+    latlng: number[];
+    demonym: string;
+    area: number;
+    gini: number;
+    timezones: string[];
+    borders: string[];
+    nativeName: string;
+    numericCode: string;
+    currencies: Currency[];
+    languages: Language[];
+    translations: Translations;
+    flag: string;
+    regionalBlocs: RegionalBloc[];
+    cioc: string;
+}
