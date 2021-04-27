5201b13b45a8c00b97be2277d239131d5ab38aea
makhatadze <vitomaxatadze@gmail.com>
2021-04-27 09:35:52 +0400

    Declare interface of language and export it.

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,10 @@ export interface Currency {
     name: string;
     symbol: string;
 }
+
+export interface Language {
+    iso639_1: string;
+    iso639_2: string;
+    name: string;
+    nativeName: string;
+}
